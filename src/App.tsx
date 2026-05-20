d2f04a7f08548f7c2bee97f03061666b597a6c5a
Gussatt <gustavoasaturnino@gmail.com>
2026-05-20 04:05:04 -0300

    feat: configure client-side routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
+import { SheetView } from './pages/SheetView';
+import { CategoryDetail } from './pages/CategoryDetail';
+import { Balances } from './pages/Balances';
+import { Performance } from './pages/Performance';
+import { DailyCalculation } from './pages/DailyCalculation';
 
 function App() {
   return (
-    <Layout>
-      <h1>Dashboard</h1>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<SheetView />} />
+          <Route path="/category/:id" element={<CategoryDetail />} />
+          <Route path="/balances" element={<Balances />} />
+          <Route path="/performance" element={<Performance />} />
+          <Route path="/daily-calculation" element={<DailyCalculation />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
