e0772110cec2520fd5f65f69db25997c11fb7c90
Gustavo Saturnino <gustavo.saturnino@braze.com>
2026-06-18 02:30:35 -0300

    feat: add initial translations dictionary

---
+++ src/context/translations.ts
@@ -0,0 +1,18 @@
+export type Language = 'pt' | 'en';
+
+export const translations = {
+  pt: {
+    'menu.title': 'Menu',
+    'menu.language': 'Idioma',
+    'menu.language.pt': 'Português',
+    'menu.language.en': 'Inglês',
+    'menu.logout': 'Sair',
+  },
+  en: {
+    'menu.title': 'Menu',
+    'menu.language': 'Language',
+    'menu.language.pt': 'Portuguese',
+    'menu.language.en': 'English',
+    'menu.logout': 'Logout',
+  }
+};
